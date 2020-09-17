2794fc3cc27c793f1470adad014417c7e9567f4b
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-09-16 17:29:10 -0700

    Added log out button event listener, functionality yet to be completed

--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
 const signUpBtn = document.getElementById('sign-up')
+const logOutBtn = document.getElementById('log-out')
 
 function confirm_delete() {
     return confirm('Are you sure you want to delete?')
@@ -6,4 +7,8 @@ function confirm_delete() {
 
 signUpBtn.addEventListener('click', () => {
   window.location.href = '/sign_up'
+})
+
+logOutBtn.addEventListener('click', () => {
+  window.location.href = '/log_out'
 })
