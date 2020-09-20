6ce479f412013ef8683581a42ef052a53f14239d
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-09-20 12:10:10 -0700

    Final testing and debugging, improvement of JavaScript sign-up confirmation.

--- static/signUpConfirm.js
+++ static/signUpConfirm.js
@@ -1,8 +1,18 @@
 const submitBtn = document.getElementById('sign-up-submit')
+const userInputs = document.querySelectorAll('input')
+const signUpForm = document.querySelector('form')
+const textNode = document.createElement('p')
+const fieldMessage = textNode.innerText = 'All fields are required.'
 
 const submitBtnHandler = () => {
-  window.location.href = '/'
-  alert('Thank you for signing up!')
+  if (!userInputs.value) {
+    signUpForm.appendChild(fieldMessage)
+    window.location.href = '/sign_up'
+  } else {
+    console.log('Made it into else')
+    alert('Thank you for signing up!')
+    window.location.href = '/'
+  }
 }
 
 submitBtn.addEventListener('click', submitBtnHandler)
